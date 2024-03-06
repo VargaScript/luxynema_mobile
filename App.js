b39d268ad572cdc162d5fdb18e0466402b0677a2
VargaScript <wolfolkvargas295478@gmail.com>
2024-03-05 19:41:02 -0700

    Reinitializing Project

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Webview from "react-native-webview";
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Webview source={{ uri: "https://luxynema.businesscodecreators.com/" }} />;
 }
 
 const styles = StyleSheet.create({
